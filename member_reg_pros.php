c5da60e0a7dbc183a92833f173789e67ec928805
Chathumi Rathnayaka <163127412+chathumi25@users.noreply.github.com>
2024-06-08 16:29:48 +0530

    member_reg_pros.php

--- member_reg_pros.php
+++ member_reg_pros.php
@@ -10,17 +10,6 @@ $last_name = "";
 $birthday = "";
 $email = "";
 
-// Function to check if Member ID or Email already exists
-function checkExistence($database, $member_id, $email) {
-    $stmt = $database->prepare("SELECT * FROM member WHERE member_id = ? OR email = ?");
-    $stmt->bind_param("ss", $member_id, $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $exists = $result->num_rows > 0;
-    $stmt->close();
-    return $exists;
-}
-
 // Saving new member details
 if (isset($_POST['save'])) {
     $member_id = $_POST['member_id'];
@@ -80,6 +69,33 @@ if (isset($_GET['edit'])) {
 
     $stmt->close();
 }
+// Updating a member
+if (isset($_POST['update'])) {
+    $member_id = $_POST['member_id'];
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $birthday = $_POST['birthday'];
+    $email = $_POST['email'];
+
+    // Check for existence but allow current member_id and email to pass
+    $stmt = $database->prepare("SELECT * FROM member WHERE (member_id = ? OR email = ?) AND member_id != ?");
+    $stmt->bind_param("sss", $member_id, $email, $member_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        $_SESSION['message'] = "Member ID or Email already exists!";
+        $_SESSION['msg_type'] = "danger";
+        header("Location: member_req_index.php");
+    } else {
+        $stmt = $database->prepare("UPDATE member SET first_name = ?, last_name = ?, birthday = ?, email = ? WHERE member_id = ?");
+        $stmt->bind_param("sssss", $first_name, $last_name, $birthday, $email, $member_id);
+        $stmt->execute();
+        $stmt->close();
+
+        $_SESSION['message'] = "Record has been updated!";
+        $_SESSION['msg_type'] = "warning";
+        header("Location: member_req_index.php");
+    }
 
     $stmt->close();
 }
