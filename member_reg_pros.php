fbcf01f4c4b6e2a4ca8560df4791e752933d3e86
Chathumi Rathnayaka <163127412+chathumi25@users.noreply.github.com>
2024-06-08 22:57:26 +0530

    Edit and Update a member

--- member_reg_pros.php
+++ member_reg_pros.php
@@ -57,4 +57,55 @@ if (isset($_GET['delete'])) {
     $_SESSION['msg_type'] = "danger";
     header("Location: member_req_index.php");
 }
+// Editing a member
+if (isset($_GET['edit'])) {
+    $member_id = $_GET['edit'];
+    $update = true;
+
+    $stmt = $database->prepare("SELECT * FROM member WHERE member_id = ?");
+    $stmt->bind_param("s", $member_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $member_id = $row['member_id'];
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+        $birthday = $row['birthday'];
+        $email = $row['email'];
+    }
+
+    $stmt->close();
+}
+
+// Updating a member
+if (isset($_POST['update'])) {
+    $member_id = $_POST['member_id'];
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $birthday = $_POST['birthday'];
+    $email = $_POST['email'];
+
+    // Check for existence but allow current member_id and email to pass
+    $stmt = $database->prepare("SELECT * FROM member WHERE (member_id = ? OR email = ?) AND member_id != ?");
+    $stmt->bind_param("sss", $member_id, $email, $member_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        $_SESSION['message'] = "Member ID or Email already exists!";
+        $_SESSION['msg_type'] = "danger";
+        header("Location: member_req_index.php");
+    } else {
+        $stmt = $database->prepare("UPDATE member SET first_name = ?, last_name = ?, birthday = ?, email = ? WHERE member_id = ?");
+        $stmt->bind_param("sssss", $first_name, $last_name, $birthday, $email, $member_id);
+        $stmt->execute();
+        $stmt->close();
+
+        $_SESSION['message'] = "Record has been updated!";
+        $_SESSION['msg_type'] = "warning";
+        header("Location: member_req_index.php");
+    }
+    $stmt->close();
+}
 ?>
